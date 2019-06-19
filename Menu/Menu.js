7dd7ed9c36d0c13059cee761f198d707a7775c35
Bryan Szendel <bryan.szendel@gmail.com>
2019-06-19 17:15:19 -0600

    WIP Stretch - menu closer is working, sort of

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -11,7 +11,12 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 
+const articlesClick = document.querySelector('.articles');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', function() {
-  toggleMenu();
-});
+menuButton.addEventListener('click', () => toggleMenu());
+
+menuButton.addEventListener('click', () => TweenMax.fromTo(menu, .5, {x:-350}, {x:0}));
+
+// articlesClick.addEventListener('click', () => TweenMax.fromTo(menu, .5, {x:0}, {x:-350}));
+// the above closes the menu but it is a TERRIBLE experience because it will always do it. I need to figure out some logic to include so that it only fires if the menu is open
